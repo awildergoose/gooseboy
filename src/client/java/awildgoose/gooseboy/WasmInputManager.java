65931ddd25cf0fa63624ede7c454955bf053f5ce
awildergoose <61917452+awildergoose@users.noreply.github.com>
2025-11-04 14:07:42 +0200

    chore: fix input mouse positions

--- src/client/java/awildgoose/gooseboy/WasmInputManager.java
+++ src/client/java/awildgoose/gooseboy/WasmInputManager.java
@@ -1,7 +1,9 @@
 package awildgoose.gooseboy;
 
+import awildgoose.gooseboy.screen.WasmScreen;
 import net.minecraft.client.Minecraft;
 import org.lwjgl.glfw.GLFW;
+
 
 public class WasmInputManager {
 	private static final int MAX_KEYS = GLFW.GLFW_KEY_LAST;
@@ -33,8 +35,9 @@ public class WasmInputManager {
 
 	public static int getMouseXInFramebuffer() {
 		int width = Minecraft.getInstance().getWindow().getScreenWidth() / 2;
-		double mouseX = Minecraft.getInstance().mouseHandler.xpos() * (double) width / Minecraft.getInstance().getWindow().getScreenWidth();
-		int fbX = (int) (mouseX - (((width - Gooseboy.FRAMEBUFFER_WIDTH) / 2) + 5));
+		double mouseX =
+				Minecraft.getInstance().mouseHandler.xpos() * (double) width / (double)Minecraft.getInstance().getWindow().getScreenWidth();
+		int fbX = (int)(mouseX - (((width - WasmScreen.IMAGE_WIDTH) / 2) + 5));
 		if (fbX < 0) fbX = 0;
 		if (fbX >= Gooseboy.FRAMEBUFFER_WIDTH) fbX = Gooseboy.FRAMEBUFFER_WIDTH - 1;
 		return fbX;
@@ -42,8 +45,9 @@ public class WasmInputManager {
 
 	public static int getMouseYInFramebuffer() {
 		int height = Minecraft.getInstance().getWindow().getScreenHeight() / 2;
-		double mouseY = Minecraft.getInstance().mouseHandler.ypos() * (double) height / Minecraft.getInstance().getWindow().getScreenHeight();
-		int fbY = (int) (mouseY - (((height - Gooseboy.FRAMEBUFFER_HEIGHT) / 2) + 5));
+		double mouseY =
+				Minecraft.getInstance().mouseHandler.ypos() * (double) height / (double) Minecraft.getInstance().getWindow().getScreenHeight();
+		int fbY = (int)(mouseY - (((height - WasmScreen.IMAGE_HEIGHT) / 2) + 5));
 		if (fbY < 0) fbY = 0;
 		if (fbY >= Gooseboy.FRAMEBUFFER_HEIGHT) fbY = Gooseboy.FRAMEBUFFER_HEIGHT - 1;
 		return fbY;
